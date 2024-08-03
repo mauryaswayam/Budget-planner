769d620ade691f6f37735e274045dda6e20c3b75
Swayam maurya <138699255+mauryaswayam@users.noreply.github.com>
2024-08-03 19:57:31 +0530

    Update login.component.ts

--- src/app/budget-planner/login/login.component.ts
+++ src/app/budget-planner/login/login.component.ts
@@ -19,9 +19,9 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   registerForm!: FormGroup;
 
-  myEmail: any
-  myPassword: any
-  myUsername: any
+  myEmail: any;
+  myPassword: any;
+  myUsername: any;
 
   submittedEmail: string = '';
 
@@ -39,18 +39,18 @@ export class LoginComponent implements OnInit {
       const email = this.loginForm.get('email')?.value;
       const password = this.loginForm.get('password')?.value;
 
-      if(email == 'admin@gmail.com' && password == 12345){
-      this.router.navigate(['dashboard']);
-    } else {
-      alert('Invalid Email or Password.');
+      if (email === 'admin@gmail.com' && password === '12345') {
+        this.router.navigate(['dashboard']);
+      } else {
+        alert('Invalid Email or Password.');
+      }
     }
   }
-}
 
   onLoginSubmit() {
     // Handle login form submission
     console.log('Login form submitted', this.loginForm.value);
-    this.router.navigate(['dashboard']);
+    this.login();
   }
 
   onNavigateLogin() {
